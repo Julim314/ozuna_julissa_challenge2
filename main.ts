94410e42c82e555fa68f8e879d4648ea4e5040c8
Julim314 <72747996+Julim314@users.noreply.github.com>
2020-12-22 09:41:44 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+while (true) {
+    console.log("Light Levels:" + input.lightLevel())
+    if (input.lightLevel() < 10) {
+        light.setAll(light.rgb(0, 0, 255))
+    } else if (input.lightLevel() < 2) {
+        light.setAll(light.rgb(255, 255, 0))
+    } else {
+        light.clear()
+    }
+    
+}
