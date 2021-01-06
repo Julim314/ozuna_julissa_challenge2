c5fac92ac68eca59928caff35c548674c487de59
Julim314 <72747996+Julim314@users.noreply.github.com>
2021-01-05 20:38:10 -0500

    the thresholds were changed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 while True:
     print("Light Levels:"+ input.light_level())
     if input.light_level() < 10:
-        light.set_all (light.rgb (0,0,255))
+        light.set_all (light.rgb (255,100,20))
     elif input. light_level()<2:
         light.set_all(light.rgb(255,255,0))
     else:
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 while (true) {
     console.log("Light Levels:" + input.lightLevel())
     if (input.lightLevel() < 10) {
-        light.setAll(light.rgb(0, 0, 255))
+        light.setAll(light.rgb(255, 100, 20))
     } else if (input.lightLevel() < 2) {
         light.setAll(light.rgb(255, 255, 0))
     } else {
